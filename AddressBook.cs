680e4161967ca901007c1f3f93531e2e31cce01f
philipDmartin <philipnathan788@gmail.com>
2020-05-27 16:25:08 -0500

    first commit

---
+++ AddressBook.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace TryCatch
+{
+    class AddressBook
+    {
+
+    }
+}
+
---
+++ Contact.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TryCatch
+{
+    class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
